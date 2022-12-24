71e39f0672ba2f645798498b8d1110ec8e069d70
Jeremy Hanson <rockbasser88@hotmail.com>
2022-12-24 11:30:04 -0600

    finished GET route, data renders to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,23 +1,46 @@
-$(document).on(onReady);
+$(document).ready(onReady);
 
 function onReady() {
     console.log('DOM ready');
+    getAndRenderChildren();
 }
 
-// render "children" table data
-function renderChildList() {
+
+// GET(read) and render "children" table data
+function getAndRenderChildren() {
     console.log('DOM rendered');
-}
-
-// GET(read) /children
-function getChildrenData() {
-    console.log('in getChildrenData');
     // ajax call to server to GET(read)
     $.ajax({
         method: 'GET',
         url: '/children'
     }).then((response) => {
         console.log('GET response from childrenRouter', response);
+        $('#childrenTable').empty();
+        for (let child of response) {
+            if (child.naughty_or_nice) {
+                child.naughty_or_nice = 'Nice';
+                $('#childrenTable').append(`
+                <tr>
+                    <td>${child.name}</td>
+                    <td>${child.gift}</td>
+                    <td>${child.notes}</td>
+                    <td>${child.naughty_or_nice}</td>
+                </tr>
+                `);
+            } else {
+                child.naughty_or_nice = 'Naughty';
+                $('#childrenTable').append(`
+                <tr>
+                    <td>${child.name}</td>
+                    <td>${child.gift}</td>
+                    <td>${child.notes}</td>
+                    <td>${child.naughty_or_nice}</td>
+                </tr>
+                `);
+            }
+        }
+    }).catch((error) => {
+        console.log('error GET client!', error);
     })
 }
 
--- server/routes/children.router.js
+++ server/routes/children.router.js
@@ -5,6 +5,25 @@ const childrenRouter = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET(read)
+childrenRouter.get('/', (req, res) => {
+    console.log('GET childrenRouter');
+
+    let sqlQuery = `
+    SELECT * FROM "children"
+        ORDER BY "id";
+    `
+    // send query
+    pool.query(sqlQuery)
+        // wait for response from database
+        .then((dbRes) => {
+            // send back array of children objects if successful
+            res.send(dbRes.rows);
+        })
+        .catch((dbErr) => {
+            console.log('Error getting "children"!', dbErr);
+            res.sendStatus(500);
+        })
+})
 
 // POST(create)
 
